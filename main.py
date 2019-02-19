0eacaa11d57f7fcb5d3a0dd590a0776372a39563
Savong Tan <savongt@uw.edu>
2019-02-19 13:37:18 -0800

    h

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Note: import the app of what you are working on
-from initial_demo import app
+from vis_scatter_plot import app
 # from exercise1 import app
 # from exercise2 import app
 # from exercise2 import app
